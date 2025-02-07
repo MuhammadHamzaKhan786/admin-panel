efdca78ee62fe4f4d458d334e39f67c91f523316
MuhammadHamzaKhan786 <anwarhamza712@gmail.com>
2025-02-08 03:11:58 +0500

    Finalized

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,10 @@
-import { createClient } from 'next-sanity'
+import { createClient } from 'next-sanity';
 
-import { apiVersion, dataset, projectId } from '../env'
-
+// Access the environment variables from process.env
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '', // Ensure to use the public variable
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production', // Default to 'production' if not set
+  apiVersion: '2023-01-01', // You can set the API version here or use a dynamic one from your env variables
+  useCdn: true, // Set to false if statically generating pages, using ISR, or tag-based revalidation
+  token: process.env.SANITY_API_TOKEN, // Use the private API token from your environment
+});
